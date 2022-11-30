8859f18a173d92f4c56c0f1d62fb0ea2ab36ecfa
WebCoder2021 <82173844+WebCoder2021@users.noreply.github.com>
2022-11-30 21:41:10 +0500

    dfgdfg

--- home/urls.py
+++ home/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', home, name="home"),
-] 
+    path('posts/', PostList.as_view(), name="posts"),
+    path('post/<pk>/', PostDetailView.as_view(), name="post_detail"),
+
+]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import *
 
 # Create your views here.
 def home(request):
+    context = {}
+    context['posts'] = Post.objects.all()[:3]
 
-    return render(request,'index.html')
+    return render(request,'index.html',context)
+
+
+class PostList(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'blog.html'
+    queryset = Post.objects.all()
+    
+
+ 
+ 
+class PostDetailView(DetailView):
+    # specify the model to use
+    model = Post
+    template_name = 'blog-details.html'
+    context_object_name = 'post'
+    def get_context_data(self, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        context['category'] = Category.objects.all()
+        context['tags'] = Tag.objects.all()
+        return context
